1d7ed950cca413a6c94f8bb5a54124deab280e80
Thijs Nieuwdorp <thijsnieuwdorp@hotmail.com>
2026-02-19 13:12:36 +0100

    First draft user guide

--- docs/source/src/python/polars-cloud/query-profile.py
+++ docs/source/src/python/polars-cloud/query-profile.py
@@ -10,4 +10,16 @@ result.await_profile().data
 # --8<-- [start:await_summary]
 result.await_profile().summary
 # --8<-- [end:await_summary]
+
+# --8<-- [start:explain]
+result.await_result().plan()
+# --8<-- [end:explain]
+
+# --8<-- [start:explain_ir]
+result.await_result().plan("ir")
+# --8<-- [end:explain_ir]
+
+# --8<-- [start:graph]
+result.await_result().graph()
+# --8<-- [end:graph]
 """
